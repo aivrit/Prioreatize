02f20dc95e8a992c8732c23fbd94706a2b61453e
michaellevko <michaellevko@gmail.com>
2017-07-29 20:40:46 +0300

    working on google maps added ngmap dependency

--- config/assets/default.js
+++ config/assets/default.js
@@ -25,6 +25,7 @@ module.exports = {
         'public/lib/angular-ui-router/release/angular-ui-router.js',
         'public/lib/owasp-password-strength-test/owasp-password-strength-test.js',
         'public/lib/angular-route/angular-route.js',
+        'public/lib/ngmap/build/scripts/ng-map.min.js'
         // endbower
       ],
       tests: ['public/lib/angular-mocks/angular-mocks.js']
